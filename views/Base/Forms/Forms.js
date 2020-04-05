0ec1082da1bc0873d48fea7eafbeebd2d285423c
joaopfcruz <joaopfcruz@gmail.com>
2020-04-05 12:54:48 +0100

    First Commit

---
+++ views/Base/Forms/Forms.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Forms extends Component {
+
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+      </div>
+    );
+  }
+}
+
+export default Forms;
